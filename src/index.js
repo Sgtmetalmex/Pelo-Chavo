e5e74a014bffe2f35ae231dd92ee26e32c202b37
Azielongas <aziel2000@hotmail.com>
2020-08-07 17:36:11 -0600

    Prueba de conexion a app

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const morgan = require('morgan');
 const app = express();
 const path = require('path');
 const arduinosens = require('./models/arduinoSensors');
+const classroomData = require('./models/classroomData');
 const mongoose = require('mongoose');
 var url = 'mongodb+srv://admin:admin@azielongas-rztzj.mongodb.net/PArduino?retryWrites=true&w=majority';
 app.use(require('express').static(__dirname + '../node_modules/socket.io-client/dist'));
@@ -28,39 +29,36 @@ var io = require('socket.io')(server);
 io.on('connection', (socket) => {
   console.log("Socket connected");
   socket.on('roomData', (id) => {
-    var classroomData = arduinosens.find();
-    socket.emit('initialData', classroomData[id])
-    socket.join(id)
+  socket.emit('initialData', classroomData[id])
+  socket.join(id)
   });
   socket.on('arduinoUpdate', async ({ arduinoUpdate }) => {
-    const datos = await arduinosens.findOne({ id: '1' });
     var searchParams = new URLSearchParams(arduinoUpdate);
     for (let [key, value] of searchParams) {
       console.log(key, value);
       switch (key) {
-        case "temp":
+        case "Ac":
           if (parseFloat(value) <= 20)//If temp is less than 20 we send a false cuz the temperature is not the normal temperature in Chihuahua(too cold)(red COlor)   
-          { await arduinosens.updateOne({id: '1'}, { $set: {ac: false}});}
+          { await arduinosens.updateOne({id: '1'}, { $set: {ac: false}}); classroomData.Ac = false; }
           else 
-          { await arduinosens.updateOne({id: '1'}, { $set: {ac: true}});}
-        case "dstnc":
+          { await arduinosens.updateOne({id: '1'}, { $set: {ac: true}}); classroomData.Ac = true; }
+        case "Windows":
           if (parseFloat(value) <= 5)//If dstnc is less than 5 we send a true cuz the windows are closed(Green color) 
-          { await arduinosens.updateOne({id: '1'}, { $set: {window: true}});}
+          { await arduinosens.updateOne({id: '1'}, { $set: {window: true}}); classroomData.Windows = true; }
           else 
-          { await arduinosens.updateOne({id: '1'}, { $set: {window: false}});}
-        case "light":
+          { await arduinosens.updateOne({id: '1'}, { $set: {window: false}}); classroomData.Windows = false; }
+        case "lights":
           if (parseFloat(value) <= 400)//if light is less than 400 it is enviromental light and its ok so we send a true(Green Color) 
-          {await arduinosens.updateOne({id: '1'}, { $set: {light: true }});}
+          {await arduinosens.updateOne({id: '1'}, { $set: {light: true }}); classroomData.Lights = true;  }
           else
-          { await arduinosens.updateOne({id: '1'}, { $set: {light: false }});}
-        case "Wifi":
+          { await arduinosens.updateOne({id: '1'}, { $set: {light: false }}); classroomData.Lights = false;  }
+        case "Proyector":
           if (parseInt(value) === 1)//If Wifi is on the, proyector is On too so we send a false(Red Color)
-          {await arduinosens.updateOne({id: '1'},{$set: {wifi: false}});}
+          {await arduinosens.updateOne({id: '1'},{$set: {wifi: false}}); classroomData.Proyector = false; }
           else
-          {await arduinosens.updateOne({id: '1'},{$set: {wifi: true}});} 
+          {await arduinosens.updateOne({id: '1'},{$set: {wifi: true}}); classroomData.Proyector = true; } 
       }
-        //socket.to(id).emit("updateData", classroomData[id]);
+      socket.to(id).emit("updateData", classroomData[id]);
     }
   });
 });
-
---
+++ src/models/classroomData.js
@@ -0,0 +1,7 @@
+var classroomData = {
+      Windows: Boolean,
+      Lights: Boolean,
+      Ac: Boolean,
+      Proyector: Boolean
+    }
+    module.exports = classroomData;
